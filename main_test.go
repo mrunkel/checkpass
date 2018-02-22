cedc24e5f12715ce572a5d872155742e93d51502
Marc Runkel <marc@runkel.org>
2018-02-22 18:33:21 +0100

    Update .gitignore, add first test

---
+++ main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCheckHIBP(t *testing.T) {
+
+	tables := []struct {
+		password string
+		expectedCount int
+	} {
+		{"ilovemymom", 20141},
+		{"apostasy-fryer-osaka-impiety-aspirin-hightail", 0},
+	}
+
+	for _, table := range tables {
+		count := checkHIBP(table.password)
+
+		if count != table.expectedCount {
+			t.Errorf("checkHIBP was incorrect, got: %d, want: %d for %s.", count, table.expectedCount, table.password)
+		}
+
+	}
+
+
+}
